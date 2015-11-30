99ba25d33821d20cb8ff20d979d4eaab14681cf4
stefano kitano <S.Kouroupis@gmail.com>
2015-11-30 23:00:24 +0200

    small changes

--- src/Openlayers/geometry.js
+++ src/Openlayers/geometry.js
@@ -1,61 +1,55 @@
+//needs primitives.js
+/*global CreateOpenLayersPoint:false */
 //a polygon simplification algorithm I wrote based on a tolerance value
-function Simplify(feature, tolerance){
-    var polygon = features[0].geometry.components[0];
-    var truePolygon = polygon.getVertices();
+function Simplify(features, tolerance) {
+    var truePolygon = features[0].geometry.components[0].getVertices();
     var simplefiedPolygon = [];
 
-    simplefiedPolygon[0] = truePolygon[0];
+    simplefiedPolygon[0] = CreateOpenLayersPoint(truePolygon[0].x, truePolygon[0].y);
 
     var ivertex = 0;
     var ipoint = 0;
 
     for (var c = 0; c < truePolygon.length - 2; c++) {
-        var point1 = [];
+      var point1 = CreateOpenLayersPoint(truePolygon[ivertex].x, truePolygon[ivertex].y);
+      var point2 = CreateOpenLayersPoint(truePolygon[c + 2].x, truePolygon[c + 2].y);
 
-        point1 = {
-            x: truePolygon[ivertex].x,
-            y: truePolygon[ivertex].y
-        };
+      var MidPoints = [];
 
-        var point2 = [];
-        point2 = {
-            x: truePolygon[c + 2].x,
-            y: truePolygon[c + 2].y
-        };
+      for (var j = 0; j < (c + 1 - ivertex); j++) {
+        MidPoints[j] = truePolygon[ivertex + j + 1];
+      }
 
-        var MidPoints = [];
+    var D = Math.sqrt(Math.pow((point1.x - point2.x), 2) + Math.pow((point1.y - point2.y), 2));
+    var y1my2 = (point1.y - point2.y);
+    var x2mx1 = (point2.x - point1.x);
+    var C = (point2.y * point1.x) - (point1.y * point2.x);
 
-        for (var j = 0; j < (c + 1 - ivertex); j++) {
-            MidPoints[j] = truePolygon[ivertex + j + 1];
-        }
+    var run = 1;
 
-        var D = Math.sqrt(Math.pow((point1.x - point2.x), 2) + Math.pow((point1.y - point2.y), 2));
-        var y1my2 = (point1.y - point2.y);
-        var x2mx1 = (point2.x - point1.x);
-        var C = (point2.y * point1.x) - (point1.y * point2.x);
-
-        var run = 1;
-
-        for (var i = 0; i < MidPoints.length; i++) {
-            var dist = Math.abs(MidPoints[i].x * y1my2 + MidPoints[i].y * x2mx1 + C) / D;
-            if (dist > tolerance) {
-                run = -1;
-            }
-        }
-
-        if (run == -1) {
-            ipoint++;
-            ivertex = c + 1;
-            simplefiedPolygon[ipoint] = truePolygon[c + 1];
-        }
+    for (var i = 0; i < MidPoints.length; i++) {
+      var dist = Math.abs(MidPoints[i].x * y1my2 + MidPoints[i].y * x2mx1 + C) / D;
+      if (dist > tolerance) {
+        run = -1;
+      }
     }
-    simplefiedPolygon[ipoint + 1] = truePolygon[truePolygon.length - 2];
-    simplefiedPolygon[ipoint + 2] = truePolygon[truePolygon.length - 1];
 
-    var ring = new OpenLayers.Geometry.LinearRing(simplefiedPolygon);
-    var polygon = new OpenLayers.Geometry.Polygon([ring]);
-    var attributes = {name: "my name", bar: "foo"};
-    var newfeature = new OpenLayers.Feature.Vector(polygon, attributes);
+    if (run == -1) {
+      ipoint++;
+      ivertex = c + 1;
+      simplefiedPolygon[ipoint] = CreateOpenLayersPoint(truePolygon[c + 1].x, truePolygon[c + 1].y);
+    }
+  }
+  simplefiedPolygon[ipoint + 1] = truePolygon[truePolygon.length - 2];
+  simplefiedPolygon[ipoint + 2] = truePolygon[truePolygon.length - 1];
 
-    return newfeature;
+  var ring = new OpenLayers.Geometry.LinearRing(simplefiedPolygon);
+  var polygon = new OpenLayers.Geometry.Polygon([ring]);
+  var attributes = {
+    name: "my name",
+    bar: "foo"
+  };
+  var newfeature = new OpenLayers.Feature.Vector(polygon, attributes);
+
+  return newfeature;
 }
