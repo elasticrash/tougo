f1303cc6a6d818aefeb1d4df64d3896d442080f6
stefano kitano <S.Kouroupis@gmail.com>
2016-03-30 00:12:40 +0100

    optimisation

--- src/original/map.js
+++ src/original/map.js
@@ -21,117 +21,132 @@ var wmsDisplay = function(canvasId) {
     var tileWidthInMeters;
     var tileHeightInMeters;
 
-    //Predefined ZoomLevels
-    var MapZoomObject = [
-        {
-            scale: 300000000,
-            pixelSize: 79375.1587503175
-        },
-        {
-            scale: 100000000,
-            pixelSize: 26458.386250105796
-        },
-        {
-            scale: 30000000,
-            pixelSize: 7937.51587503175
-        },
-        {
-            scale: 10000000,
-            pixelSize: 2645.8386250105796
-        },
-        {
-            scale: 3000000,
-            pixelSize: 793.751587503175
-        },
-        {
-            scale: 1000000,
-            pixelSize: 264.583862501058
-        },
-        {
-            scale: 300000,
-            pixelSize: 79.3751587503175
-        },
-        {
-            scale: 100000,
-            pixelSize: 26.4583862501058
-        },
-        {
-            scale: 50000,
-            pixelSize: 13.2291931250529
-        },
-        {
-            scale: 20000,
-            pixelSize: 5.29167725002117
-        },
-        {
-            scale: 10000,
-            pixelSize: 2.64583862501058
-        },
-        {
-            scale: 5000,
-            pixelSize: 1.32291931250529
-        },
-        {
-            scale: 2500,
-            pixelSize: 0.661459656252646
-        },
-        {
-            scale: 1000,
-            pixelSize: 0.264583862501058
-        },
-        {
-            scale: 500,
-            pixelSize: 0.132291931250529
-        },
-        {
-            scale: 250,
-            pixelSize: 0.0661459656252645
-        },
-        {
-            scale: 125,
-            pixelSize: 0.03307298281263225
-        },
-        {
-            scale: 62.5,
-            pixelSize: 0.016536491406316123
-        },
-        {
-            scale: 31.25,
-            pixelSize: 0.008268245703158062
-        },
-        {
-            scale: 15.625,
-            pixelSize: 0.004134122851579031
-        },
-        {
-            scale: 7.8125,
-            pixelSize: 0.0020670614257895154
-        },
-        {
-            scale: 3.90625,
-            pixelSize: 0.0010335307128947577
-        },
-        {
-            scale: 1.953125,
-            pixelSize: 0.0005167653564473789
-        }
-    ];
-
-    //Initial Scale Level
-    var currentScale = 6;
-
     //Our Canvas Object
     var canvas = document.getElementById(canvasId);
     var ctx = canvas.getContext("2d");
 
 
-    //get The pixelSize of the Requested Level
-    function getPixelSize(zoomLevel) {
-        return MapZoomObject[zoomLevel].pixelSize;
-    }
+    //get The pixelSize of the Requested Level, closure pattern
+    var mapAttributes = function(zoomLevel) {
+        //Initial Scale Level
+        var currentScale = 6;
+        //Predefined ZoomLevels
+        var MapZoomObject = [
+            {
+                scale: 300000000,
+                pixelSize: 79375.1587503175
+            },
+            {
+                scale: 100000000,
+                pixelSize: 26458.386250105796
+            },
+            {
+                scale: 30000000,
+                pixelSize: 7937.51587503175
+            },
+            {
+                scale: 10000000,
+                pixelSize: 2645.8386250105796
+            },
+            {
+                scale: 3000000,
+                pixelSize: 793.751587503175
+            },
+            {
+                scale: 1000000,
+                pixelSize: 264.583862501058
+            },
+            {
+                scale: 300000,
+                pixelSize: 79.3751587503175
+            },
+            {
+                scale: 100000,
+                pixelSize: 26.4583862501058
+            },
+            {
+                scale: 50000,
+                pixelSize: 13.2291931250529
+            },
+            {
+                scale: 20000,
+                pixelSize: 5.29167725002117
+            },
+            {
+                scale: 10000,
+                pixelSize: 2.64583862501058
+            },
+            {
+                scale: 5000,
+                pixelSize: 1.32291931250529
+            },
+            {
+                scale: 2500,
+                pixelSize: 0.661459656252646
+            },
+            {
+                scale: 1000,
+                pixelSize: 0.264583862501058
+            },
+            {
+                scale: 500,
+                pixelSize: 0.132291931250529
+            },
+            {
+                scale: 250,
+                pixelSize: 0.0661459656252645
+            },
+            {
+                scale: 125,
+                pixelSize: 0.03307298281263225
+            },
+            {
+                scale: 62.5,
+                pixelSize: 0.016536491406316123
+            },
+            {
+                scale: 31.25,
+                pixelSize: 0.008268245703158062
+            },
+            {
+                scale: 15.625,
+                pixelSize: 0.004134122851579031
+            },
+            {
+                scale: 7.8125,
+                pixelSize: 0.0020670614257895154
+            },
+            {
+                scale: 3.90625,
+                pixelSize: 0.0010335307128947577
+            },
+            {
+                scale: 1.953125,
+                pixelSize: 0.0005167653564473789
+            }
+        ];
+
+        //current PixelSize
+        var currentPixelSize;
+
+        return {
+            getPixelSize: function(zoomLevel){
+                currentPixelSize = MapZoomObject[zoomLevel].pixelSize;
+            },
+            changeCurrentScaleBy:function(inc){
+                currentScale+=inc;
+            },
+            getCurrentScale:function(){
+                return currentScale;
+            },
+            getCurrentPixelSize:function(){
+                return currentPixelSize;
+            }
+        };
+    }();
 
     //The pixelSize of the current Level
-    var currentPixelSize = getPixelSize(currentScale);
+    mapAttributes.getPixelSize(mapAttributes.getCurrentScale());
 
     //Initial and Current map Upper Left Point
     //Initial and Current map Lower Left Point
@@ -140,8 +155,8 @@ var wmsDisplay = function(canvasId) {
         y: 4300000
     };
     var mapLLPoint = {
-        x: mapULPoint.x + ctx.canvas.width * currentPixelSize,
-        y: mapULPoint.y - ctx.canvas.height * currentPixelSize
+        x: mapULPoint.x + ctx.canvas.width * mapAttributes.getCurrentPixelSize(),
+        y: mapULPoint.y - ctx.canvas.height * mapAttributes.getCurrentPixelSize()
     };
 
     //event for coordinates
@@ -160,8 +175,8 @@ var wmsDisplay = function(canvasId) {
         var x = event.pageX - canvasLeft;
         var y = event.pageY - canvasTop;
         return {
-            x: Math.round((mapULPoint.x + x * currentPixelSize)*100)/100,
-            y: Math.round((mapULPoint.y - y * currentPixelSize)*100)/100
+            x: Math.round((mapULPoint.x + x * mapAttributes.getCurrentPixelSize())*100)/100,
+            y: Math.round((mapULPoint.y - y * mapAttributes.getCurrentPixelSize())*100)/100
         };
     }
 
@@ -176,11 +191,11 @@ var wmsDisplay = function(canvasId) {
                 x: local.x - pan_point.x,
                 y: local.y - pan_point.y
             };
-
-            mapULPoint.x = mapULPoint.x + moveDifferencePoint.x;
-            mapULPoint.y = mapULPoint.y + moveDifferencePoint.y;
-            mapLLPoint.x = mapLLPoint.x + moveDifferencePoint.x;
-            mapLLPoint.y = mapLLPoint.y + moveDifferencePoint.y;
+            pan_point = CalculateCursonPosition(evt);
+            mapULPoint.x = mapULPoint.x - moveDifferencePoint.x;
+            mapULPoint.y = mapULPoint.y - moveDifferencePoint.y;
+            mapLLPoint.x = mapLLPoint.x - moveDifferencePoint.x;
+            mapLLPoint.y = mapLLPoint.y - moveDifferencePoint.y;
             FullRefresh();
         }
     }, false);
@@ -221,7 +236,8 @@ var wmsDisplay = function(canvasId) {
             tiles.forEach(function (tile) {
                 globalLayerList.forEach(function (layer, j){
                     if (layer.visible === true) {
-                        if (currentScale <= layer.MinVisibleScale && currentScale >= layer.MaxVisibleScale) {
+                        if (mapAttributes.getCurrentScale() <= layer.MinVisibleScale &&
+                            mapAttributes.getCurrentScale() >= layer.MaxVisibleScale) {
                             var tileAttributes = {};
 
                             tileNameComponents = tile.split("~");
@@ -256,16 +272,16 @@ var wmsDisplay = function(canvasId) {
     function mapCalculations() {
         var returnObject = {};
 
-        tileWidthInMeters = tileWidth * currentPixelSize;
-        tileHeightInMeters = tileHeight * currentPixelSize;
+        tileWidthInMeters = tileWidth * mapAttributes.getCurrentPixelSize();
+        tileHeightInMeters = tileHeight * mapAttributes.getCurrentPixelSize();
         returnObject.howManyTilesFromWE = Math.floor((mapULPoint.x - WestEnd) / tileWidthInMeters);
         returnObject.distanceFromOriginWE = WestEnd + returnObject.howManyTilesFromWE * tileWidthInMeters;
         returnObject.howManyTilesFromNE = Math.floor((NorthEnd - mapULPoint.y) / tileHeightInMeters);
         returnObject.distanceFromOriginNE = NorthEnd - returnObject.howManyTilesFromNE * tileHeightInMeters;
         returnObject.noOfTilesAtWidth = Math.floor((NorthEnd - mapLLPoint.y) / tileHeightInMeters) - returnObject.howManyTilesFromNE + 1;
         returnObject.noOfTilesAtHeight = Math.floor((mapLLPoint.x - WestEnd) / tileWidthInMeters) - returnObject.howManyTilesFromWE + 1;
-        returnObject.howManyPixelsFromWE = -Math.round(Math.abs((returnObject.distanceFromOriginWE - mapULPoint.x) / currentPixelSize));
-        returnObject.howManyPixelsFromNE = -Math.round(Math.abs((mapULPoint.y - returnObject.distanceFromOriginNE) / currentPixelSize));
+        returnObject.howManyPixelsFromWE = -Math.round(Math.abs((returnObject.distanceFromOriginWE - mapULPoint.x) / mapAttributes.getCurrentPixelSize()));
+        returnObject.howManyPixelsFromNE = -Math.round(Math.abs((mapULPoint.y - returnObject.distanceFromOriginNE) / mapAttributes.getCurrentPixelSize()));
 
         return returnObject;
     }
@@ -334,33 +350,36 @@ var wmsDisplay = function(canvasId) {
     }
 
     function RecalculateCanvasBBox(midpoint){
-        mapULPoint.x = midpoint.x - (canvas.width/2*currentPixelSize);
-        mapULPoint.y = midpoint.y + (canvas.height/2*currentPixelSize);
-        mapLLPoint.x = midpoint.x + (canvas.width/2*currentPixelSize);
-        mapLLPoint.y = midpoint.y - (canvas.height/2*currentPixelSize);
+        mapULPoint.x = midpoint.x - (canvas.width/2*mapAttributes.getCurrentPixelSize());
+        mapULPoint.y = midpoint.y + (canvas.height/2*mapAttributes.getCurrentPixelSize());
+        mapLLPoint.x = midpoint.x + (canvas.width/2*mapAttributes.getCurrentPixelSize());
+        mapLLPoint.y = midpoint.y - (canvas.height/2*mapAttributes.getCurrentPixelSize());
     }
 
     //Toolbar Events
     function Toolbar_Events(){
-        //$('#tool_pan').on("click", function () {
-        //    canvas.addEventListener("mousedown", function (evt) {
-        //        pan_point = CalculateCursonPosition(evt);
-        //        pan_tool = true;
-        //    }, false);
-        //    canvas.addEventListener("mouseup", function (evt) {
-        //        pan_tool = false;
-        //    }, false);
-        //});
+        $('#tool_pan').on("click", function () {
+            canvas.addEventListener("mousedown", function (evt) {
+                pan_point = CalculateCursonPosition(evt);
+                pan_tool = true;
+            }, false);
+            canvas.addEventListener("mouseup", function (evt) {
+                pan_tool = false;
+                $('#tool_pan').removeEventListener('click');
+                canvas.removeEventListener('mousedown');
+
+            }, false);
+        });
         $('#tool_zoom_in').on("click", function () {
-            currentScale+=1;
-            currentPixelSize = getPixelSize(currentScale);
+            mapAttributes.changeCurrentScaleBy(1);
+            mapAttributes.getPixelSize(mapAttributes.getCurrentScale());
             var midpoint = GetMapMidPoint();
             RecalculateCanvasBBox(midpoint);
             FullRefresh();
         });
         $('#tool_zoom_out').on("click", function () {
-            currentScale-=1;
-            currentPixelSize = getPixelSize(currentScale);
+            mapAttributes.changeCurrentScaleBy(-1);
+            mapAttributes.getPixelSize(mapAttributes.getCurrentScale());
             var midpoint = GetMapMidPoint();
             RecalculateCanvasBBox(midpoint);
             FullRefresh();
